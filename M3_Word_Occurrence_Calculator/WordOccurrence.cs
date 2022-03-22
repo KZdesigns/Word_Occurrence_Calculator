480ae9e5706bffb07e3e891206fc6e30d2f32a75
Kyle Zimmerman <kaz0010@auburn.com>
2022-03-21 20:21:29 -0700

    WordOccurence class created it has two fields word and count

---
+++ M3_Word_Occurrence_Calculator/WordOccurrence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3_Word_Occurrence_Calculator
+{
+    class WordOccurrence
+    {
+
+        private String Word { get; set; }
+        private int Count { get; set; }
+
+
+    }
+}
---
+++ WordOccurrence/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordOccurrence
+{
+    public class WordOccurrence
+    {
+
+        private String Word { get; set; }
+        private int Count { get; set; }
+
+    }
+}
